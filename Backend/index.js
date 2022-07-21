65d9ce9ae6dd0fb20ad48c8eba5f5365183d33a1
MirajChathuranga <mirajmanage@gmail.com>
2022-07-21 11:43:08 +0530

    backend update

--- Backend/index.js
+++ Backend/index.js
@@ -24,3 +24,25 @@ mongoose.connect(process.env.MONGO_URL, {
 const server = app.listen(process.env.PORT, ()=>{
     console.log(`Server started on port ${process.env.PORT}`);
 });
+
+const io = socket(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      credentials: true,
+    },
+  });
+  
+  global.onlineUsers = new Map();
+  io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+      onlineUsers.set(userId, socket.id);
+    });
+  
+    socket.on("send-msg", (data) => {
+      const sendUserSocket = onlineUsers.get(data.to);
+      if (sendUserSocket) {
+        socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+      }
+    });
+  });
